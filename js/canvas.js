93a2cb2105e787282253ccebff1ba54a7efe1c97
aparnalovestocode@gmail.com <aparna@midnight.mynetworksettings.com>
2024-11-05 20:59:54 -0500

    formatting and drawable canvas

--- js/canvas.js
+++ js/canvas.js
@@ -1,14 +1,16 @@
-window.onload =  init;
+window.onload = init;
 
 var canvas, ctx;
+var drawing = false;
+function init() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    canvas.addEventListener('mousedown', () => { drawing = true }, false)
+    canvas.addEventListener('mouseup', () => { drawing = false }, false)
+    canvas.addEventListener('mousemove', (event) => draw(event), false);
 
-function init(){
- canvas = document.getElementById("canvas");
- ctx = canvas.getContext("2d");
- canvas.addEventListener('click', (event) => draw(event), false);
-
- resizeCanvas()
- window.addEventListener('resize', resizeCanvas, false);
+    resizeCanvas()
+    window.addEventListener('resize', resizeCanvas, false);
 }
 
 function resizeCanvas() {
@@ -17,7 +19,9 @@ function resizeCanvas() {
 }
 
 function draw(event) {
-    console.log(event.clientX)
-    console.log(event.clientY)
-    ctx.fillRect(event.clientX,event.clientY,10,10); // fill in 10x10 pixel grid
+    if (drawing) {
+        console.log(event.clientX)
+        console.log(event.clientY)
+        ctx.fillRect(event.clientX, event.clientY, 10, 10); // fill in 10x10 pixel grid
+    }
 }
