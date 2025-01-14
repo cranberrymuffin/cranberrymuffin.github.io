802500d0b617056764981f38e80937db40fa8037
cranberrymuffin <aparnalovestocode@gmail.com>
2025-01-14 12:14:39 -0500

    vite gh-pages config and seperatate snowstorm app:

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import Experience from './Experience'
+import Experience from './snowstorm/Experience'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
