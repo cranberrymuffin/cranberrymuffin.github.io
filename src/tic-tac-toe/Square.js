2220a3a8e5025a2e9e710eeb779a5bbf51050f58
cranberrymuffin <aparnalovestocode@gmail.com>
2025-01-15 20:28:40 -0500

    simplify tic tac toe logic

--- src/tic-tac-toe/Square.js
+++ src/tic-tac-toe/Square.js
@@ -1,35 +1,7 @@
-const Square = ({ square, id, go, setGo, cells, setCells, winningMessage }) => {
-    const handleClick = (e) => {
-        if (!winningMessage) {
-            const taken = cells[id] !== ""
-            if (!taken) {
-                if (go === "circle") {
-                    e.target.firstChild.classList.add("circle")
-                    handleCellChange("circle")
-                    setGo("cross")
-                }
-                if (go === "cross") {
-                    e.target.firstChild.classList.add("cross")
-                    handleCellChange("cross")
-                    setGo("circle")
-                }
-            }
-        }
-    }
-    const handleCellChange = (className) => {
-        const nextBoard = cells.map((cell, index) => {
-            if (index === id) {
-                return className
-            } else {
-                return cell
-            }
-        })
-        setCells(nextBoard)
-        console.log(nextBoard)
-    }
-
-    return (<div id={id} className="square" onClick={handleClick}>
-        <div>{square}</div>
+const Square = (props) => {
+    return (<div id={props.id} className="square" onClick={props.handleClick}>
+        {props.data === "cross" && <div className="cross"></div>}
+        {props.data === "circle" && <div className="circle"></div>}
     </div>);
 }
 
--- src/tic-tac-toe/TicTacToe.js
+++ src/tic-tac-toe/TicTacToe.js
@@ -3,35 +3,50 @@ import Square from './Square'
 import { useState, useEffect } from 'react';
 
 export default function TicTacToe() {
-    const [cells, setCells] = useState(["","","","","","","","",""])
+    const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""])
     const [go, setGo] = useState("circle")
     const [winningMessage, setWinningMessage] = useState(null)
 
-    const message = "it is now " + go +"'s go"
+    const message = "it is now " + go + "'s go"
 
     const checkScore = () => {
+        console.log("hello")
         const winningCombos = [
-            [0,1,2], [3,4,5], [6,7,8],[0,3,6],[1,4,7], [2,5,8], [0,4,8], [2,4,6]
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
         ]
         winningCombos.forEach(array => {
             let circleWins = array.every(cell => cells[cell] === "circle")
-            if(circleWins) {
+            if (circleWins) {
                 setWinningMessage("circle wins!")
                 return
             }
         })
         winningCombos.forEach(array => {
             let crossWins = array.every(cell => cells[cell] === "cross")
-            if(crossWins) {
+            if (crossWins) {
                 setWinningMessage("cross wins!")
                 return
             }
         })
-        
-        if(!cells.includes("")) {
+
+        if (!cells.includes("")) {
             setWinningMessage("no winner")
         }
     }
+
+    const handleClick = (index) => {
+        if (winningMessage === undefined) {
+            cells[index] = go
+            setCells(cells)
+            if (go === "cross") {
+                setGo("circle")
+            } else {
+                setGo("cross")
+            }
+            checkScore()
+        }
+    }
+
 
     useEffect(() => {
         checkScore()
@@ -41,9 +56,9 @@ export default function TicTacToe() {
         <div className="container">
             <div id="you"></div>
             <div id="opponent"></div>
-            <h1 id="info">{ winningMessage || message }</h1>
+            <h1 id="info">{winningMessage || message}</h1>
             <div id="game" className="game">
-                {cells.map((square, index) => <Square key={index} id={index} go={go} setGo={setGo} setCells={setCells} cells={cells} winningMessage={winningMessage}/>)}
+                {cells.map((square, index) => <Square key={index} data={cells[index]} handleClick={() => handleClick(index)} />)}
             </div>
         </div>
     );
