4cfa7e78705e4f37b2026db4b55baddcd632edda
cranberrymuffin <aparnalovestocode@gmail.com>
2025-01-19 19:18:59 -0500

    add nav bar

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,10 +2,19 @@ import './home.css';
 import { Canvas } from '@react-three/fiber';
 import Experience from './Experience';
 export default function Home() {
-    return (<div id="home">
-        <Canvas>
-            <color attach="background" args={["hotpink"]} />
-            <Experience />
-        </Canvas>
-    </div>)
+    return (
+        <div>
+            <div id="nav">
+                <a id="nav-element" href="/#/resume">resume</a>
+                <a  id="nav-element" href="/#/snowstorm">snowstorm</a>
+                <a  id="nav-element" href="/#/tic-tac-toe">tic-tac-toe</a>
+            </div>
+            <div id="home">
+                <Canvas>
+                    <color attach="background" args={["hotpink"]} />
+                    <Experience />
+                </Canvas>
+            </div>
+        </div>
+    )
 }
