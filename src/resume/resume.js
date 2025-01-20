909f09306d2408bf966eedf79f46bac424edd9dc
cranberrymuffin <aparnalovestocode@gmail.com>
2025-01-19 19:20:18 -0500

    resume lastname

--- src/resume/resume.js
+++ src/resume/resume.js
@@ -3,7 +3,7 @@ import './resume.css';
 export default function Resume() {
   return (<div id="main" className='main'><div className="resume" id="resume">
     <div className="business-card">
-      <h1>✨ Aparna Natarajan ✨</h1>
+      <h1>✨ Aparna ✨</h1>
       <p>
         cranberrymuffin.github.io
         <br />
