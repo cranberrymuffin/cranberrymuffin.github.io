facb15b4a3a289370a35b1e043f7227600a5bd69
cranberrymuffin <aparnalovestocode@gmail.com>
2025-01-23 14:31:27 -0500

    valentines with orbital camera

--- src/valentines/Valentines.js
+++ src/valentines/Valentines.js
@@ -1,11 +1,14 @@
 import Valentine from './valentine';
 import { Canvas } from '@react-three/fiber';
 import './home.css';
+import { OrbitControls } from '@react-three/drei';
+import { DirectionalLight } from 'three';
 
 export default function Valentines() {
 
     return (<div id="home">
         <Canvas camera={{ position: [0, 0, 50], fov: 50 }}>
+            <OrbitControls/>
             <color attach="background" args={["hotpink"]} />
             <ambientLight intensity={2} />
             <Valentine />
