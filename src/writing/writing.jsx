0042c52d732f1034f3782a0b1e75a831cd316e29
cranberrymuffin <aparnalovestocode@gmail.com>
2025-02-04 19:27:29 -0500

    tic-tac-toe blog

--- src/writing/writing.jsx
+++ src/writing/writing.jsx
@@ -1,14 +1,23 @@
-import './writing.css';
+import "./writing.css";
 
 export default function Writing() {
-  return (<div id="main" className='main'>
-    <div id="writing" className='writing'>
-      <h1> Writing </h1>
-      <ul>
-        <li>January 30, 2025: <a href="/#/blog/valentine">Valentine</a></li>
-        <li>January 27, 2025: <a href="/#/blog/snowstorm">Snowstorm</a></li>        
-      </ul>
+  return (
+    <div id="main" className="main">
+      <div id="writing" className="writing">
+        <h1> Writing </h1>
+        <ul>
+          <li>
+            January 30, 2025: <a href="/#/blog/valentine">Valentine</a>
+          </li>
+          <li>
+            January 27, 2025: <a href="/#/blog/snowstorm">Snowstorm</a>
+          </li>
+          <li>
+            February 4, 2025:{" "}
+            <a href="/#/blog/tic-tac-toe">WebRTC Tic-Tac-Toe</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
   );
 }
